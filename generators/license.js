9a6efc4d067464f7941fbc81d2c437d2427f25e2
Luis Sala <lsala@primogen.com>
2012-05-17 14:52:52 -0700

    Added Apache License Info

--- generators/license.js
+++ generators/license.js
@@ -1,6 +1,19 @@
-// ==========================================================================
-// Project:  AlfJS
-// Copyright: ©2012 Alfresco Software, Inc. and contributors.
-// License:   Licensed under LGPL license (see http://www.gnu.org/licenses/lgpl.html)
-// ==========================================================================
+/*
+Copyright (C) 2012 Alfresco Software Limited
 
+This file is part of an unsupported extension to Alfresco.
+
+Alfresco Software Limited licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
